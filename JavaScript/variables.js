58f2b4de37986c63f9dbe36a7f304c712514bdfe
NEERAJ KUMAR SINGH <101538685+neeraj6511@users.noreply.github.com>
2024-02-21 16:58:22 +0000

    container config done

---
+++ JavaScript/variables.js
@@ -0,0 +1,23 @@
+const InstagramtId = 234797654
+let accountEmail = "https://github.com/neeraj6511"
+var accountPassword = "12345"
+accountCity = "Saharsa"
+let accountState ;
+
+
+// Const can`t be change
+// MetaId = 5 Note allowed
+
+accountEmail = "neerajraj252@gmail.com"
+accountPassword = "765432"
+accountCity ="Patna"
+
+// console.log(MetaId);
+
+/*
+Prefer not used  vare 
+because of issue in block scopr and functional scope 
+*/
+
+
+console.table([InstagramtId,accountEmail,accountPassword,accountCity,accountState])
